1b0e056cbdd7050d1c3716dca053a4547ee12c4a
DWan Orchestrator <dwansystem@antigravity.ai>
2026-04-09 14:49:59 +0700

    feat(db): enable Database Seeding for Production environments to prepopulate Admin Account

--- BachDongQuan_2123110434/Program.cs
+++ BachDongQuan_2123110434/Program.cs
@@ -72,6 +72,7 @@ if (app.Environment.IsProduction())
     using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
+    SeedData.Initialize(db);
 }
 
 if (app.Environment.IsDevelopment())
